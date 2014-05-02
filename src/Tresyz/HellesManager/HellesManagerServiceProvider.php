3acda047ef829c9755a6cc39b01fdaa7b846e2fd
Henrique Boaventura <hboaventura@gmail.com>
2014-05-02 16:13:53 -0300

    fix bug

--- src/Tresyz/HellesManager/HellesManagerServiceProvider.php
+++ src/Tresyz/HellesManager/HellesManagerServiceProvider.php
@@ -31,22 +31,25 @@ class HellesManagerServiceProvider extends ServiceProvider {
 	 */
 	public function register()
 	{
-		$this->app['admin.add.user'] = $this->app->share(function() {
+    $generator = new \Way\Generators\Generators\ResourceGenerator($this->app['files']);
+    $cache = new \Way\Generators\Cache($this->app['files']);
+
+    $this->app['admin.add.user'] = $this->app->share(function() {
       return new Commands\AddUser();
     });
     $this->app['admin.remove.user'] = $this->app->share(function() {
       return new Commands\RemoveUser();
     });
-    $this->app['admin.scaffold'] = $this->app->share(function($app) {
-
-      return new Commands\Scaffold();
+    $this->app['admin.scaffold'] = $this->app->share(function() use($generator, $cache) {
+        
+      return new Commands\Scaffold($generator, $cache);
     });
 
-    $this->app['admin.generate.view'] = $this->app->share(function($app) {
+    $this->app['admin.generate.view'] = $this->app->share(function() use ($generator) {
 
       return new Commands\ViewGenerator($generator);
     });
-
+    
     $this->commands('admin.add.user');
     $this->commands('admin.remove.user');
     $this->commands('admin.scaffold');
