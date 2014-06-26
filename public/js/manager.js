8b928be180b10e66750c8fdeae10744f5434e30d
Henrique Boaventura <hboaventura@gmail.com>
2014-06-26 16:44:19 -0300

    fix mask issue and controller mispeling

--- public/js/manager.js
+++ public/js/manager.js
@@ -50,10 +50,7 @@ $(document).ready(function(){
   $('textarea').autosize();
   
   /* ---------- Masked Input ---------- */
-  $("#date").mask("99/99/9999");
-  $("#phone").mask("(999) 999-9999");
-  $("#tin").mask("99-9999999");
-  $("#ssn").mask("999-99-9999");
+  $(".date-mask").mask("99/99/9999");
   
   $.mask.definitions['~']='[+-]';
   $("#eyescript").mask("~9.99 ~9.99 999");
