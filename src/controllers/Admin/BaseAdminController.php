173df295bf3094fc4bc3425b4d884684f9daec93
Henrique Boaventura <hboaventura@gmail.com>
2014-07-03 11:09:48 -0300

    no need to extends controller to see a new admin.templates.layout

--- src/controllers/Admin/BaseAdminController.php
+++ src/controllers/Admin/BaseAdminController.php
@@ -6,11 +6,15 @@ class BaseAdminController extends \BaseController {
 
   protected $layout = 'helles-manager::admin.templates.layout';
 
-  public function __construct()
+  protected function setupLayout()
   {
-    \View::share('title', 'Helles - Manager');
-    \View::share('company', '3yz');
+    if ( ! is_null($this->layout))
+    {
+      if(\View::exists('admin.templates.layout')) {
+        $this->layout = 'admin.templates.layout';
+      }
+      $this->layout = \View::make($this->layout);
+    }
   }
-
 
 }
