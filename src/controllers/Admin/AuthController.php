0a3749afef460949ea0493822073d707065fe61c
Andressa <Andressa>
2016-05-05 14:18:30 -0300

    Ajuste form

--- src/controllers/Admin/AuthController.php
+++ src/controllers/Admin/AuthController.php
@@ -11,34 +11,37 @@
       $this->layout->content = \View::make('helles-manager::admin.auth.login');
     }
 
+
     public function postLogin()
     {
-      $credentials = array(
-        'email'    => base64_decode(Input::get('email')),
-        'password' => base64_decode(Input::get('password'))
-      );
+	  if(Request::ajax()){
+ 	      $credentials = array(
+ 	        'email'    => base64_decode(Input::get('email')),
+ 	        'password' => base64_decode(Input::get('password'))
+ 	      );
 
-      $remember = (Input::get('remember') == 1);
+ 	      $remember = (Input::get('remember') == 1);
+ 	      $err = array('url' => 'none');
 
-      try
-      {
-        $user = Sentry::authenticate($credentials, $remember);
-		$err = array();
-        if ($user) {
-          //return Redirect::route('admin.dashboard');
-		  $err = array('url'=> url('admin.dashboard'), 'error' => false);
-        }
-      } catch (\Cartalyst\Sentry\Users\LoginRequiredException $e) {
-        $err['error'] = 'Informe o e-mail/senha';
-      } catch (\Cartalyst\Sentry\Users\PasswordRequiredException $e) {
-        $err['error'] = 'Informe o e-mail/senha';
-      } catch (\Cartalyst\Sentry\Users\UserNotFoundException $e) {
-        $err['error'] = 'E-mail/senha inválidos';
-      } catch (\Cartalyst\Sentry\Users\UserNotActivatedException $e) {
-        $err['error'] = 'Usuário desativado';
-      }
-    //   return Redirect::route('admin.login')->withErrors(array('login' => $error));
-	  return json_encode($err);
+ 	      try
+ 	      {
+ 	        $user = Sentry::authenticate($credentials, $remember);
+ 	        if ($user) {
+ 	          //return Redirect::route('admin.dashboard');
+ 	          $err = array('url'=> url('admin.dashboard'), 'error' => false);
+ 	        }
+ 	      } catch (\Cartalyst\Sentry\Users\LoginRequiredException $e) {
+ 	        $err['error'] = 'Informe o e-mail/senha';
+ 	      } catch (\Cartalyst\Sentry\Users\PasswordRequiredException $e) {
+ 	        $err['error'] = 'Informe o e-mail/senha';
+ 	      } catch (\Cartalyst\Sentry\Users\UserNotFoundException $e) {
+ 	        $err['error'] = 'E-mail/senha inválidos';
+ 	      } catch (\Cartalyst\Sentry\Users\UserNotActivatedException $e) {
+ 	        $err['error'] = 'Usuário desativado';
+ 	      }
+ 	    //   return Redirect::route('admin.login')->withErrors(array('login' => $error));
+ 	      return Response::json($err);
+ 	  }
     }
 
     public function getLogout()
