46a4e7caf2bbac62fd32118c45284f2cb6356773
Heitor Glockner <heitor.glockner@paim.com.br>
2016-05-09 21:26:28 -0300

    AuthController working

--- src/controllers/Admin/AuthController.php
+++ src/controllers/Admin/AuthController.php
@@ -1,6 +1,6 @@
 <?php namespace Tresyz\HellesManager\Admin;
 
-  use Auth, BaseController, Form, Input, Redirect, Sentry, View, Request, Response;
+  use Auth, BaseController, Form, Input, Redirect, Sentry, View, Request, Response, URL;
 
   class AuthController extends BaseAdminController {
 
@@ -14,34 +14,35 @@
 
     public function postLogin()
     {
-	  if(Request::ajax()){
- 	      $credentials = array(
- 	        'email'    => base64_decode(Input::get('email')),
- 	        'password' => base64_decode(Input::get('password'))
- 	      );
+      if(Request::ajax()){
+          $credentials = array(
+            'email'    => base64_decode(Input::get('email')),
+            'password' => base64_decode(Input::get('password'))
+          );
 
- 	      $remember = (Input::get('remember') == 1);
- 	      $err = array('url' => 'none');
+          $remember = (Input::get('remember') == 1);
+          $err = array('url' => '');
 
- 	      try
- 	      {
- 	        $user = Sentry::authenticate($credentials, $remember);
- 	        if ($user) {
- 	          //return Redirect::route('admin.dashboard');
- 	          $err = array('url'=> url('admin'), 'error' => false);
- 	        }
- 	      } catch (\Cartalyst\Sentry\Users\LoginRequiredException $e) {
- 	        $err['error'] = 'Informe o e-mail/senha';
- 	      } catch (\Cartalyst\Sentry\Users\PasswordRequiredException $e) {
- 	        $err['error'] = 'Informe o e-mail/senha';
- 	      } catch (\Cartalyst\Sentry\Users\UserNotFoundException $e) {
- 	        $err['error'] = 'E-mail/senha inválidos';
- 	      } catch (\Cartalyst\Sentry\Users\UserNotActivatedException $e) {
- 	        $err['error'] = 'Usuário desativado';
- 	      }
- 	    //   return Redirect::route('admin.login')->withErrors(array('login' => $error));
- 	      return Response::json($err);
- 	  }
+          try
+          {
+            $user = Sentry::authenticate($credentials, $remember);
+            if ($user) {
+              $err = array('url'=> URL::route('admin.dashboard'), 'error' => false);
+            }
+          } catch (\Cartalyst\Sentry\Users\LoginRequiredException $e) {
+            $err['error'] = 'Informe o e-mail/senha';
+          } catch (\Cartalyst\Sentry\Users\PasswordRequiredException $e) {
+            $err['error'] = 'Informe o e-mail/senha';
+          } catch (\Cartalyst\Sentry\Users\UserNotFoundException $e) {
+            $err['error'] = 'E-mail/senha inválidos';
+          } catch (\Cartalyst\Sentry\Users\UserNotActivatedException $e) {
+            $err['error'] = 'Usuário desativado';
+          }
+      } else {
+        $err['error'] = 'Nenhuma requisição ajax efetuada';
+      }
+
+      return Response::json($err);
     }
 
     public function getLogout()
